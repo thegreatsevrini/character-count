64f8ff34f7121d924a6f3f975b74543211f91305
sev the dev <sevwelker@gmail.com>
2019-10-22 17:10:36 -0700

    adjusts some comments and uses code that is more DRY

--- js/character-count.js
+++ js/character-count.js
@@ -1,23 +1,36 @@
-var textBox = document.querySelector('#text');
+//gets text area
+var textArea = document.querySelector('#text');
 
-//listens for key pressing in the textBox
-textBox.addEventListener('keydown', function (event){
-    //gets value of textbox
-    var text = textBox.value;
-    //gets the character counter on the page
-    var count = document.querySelector('#character-count');
-    //grabs keycode of key being pressed
-    var x = event.keyCode;
-    //if x equals 8 (the delete key) minus one from the text length
-    //otherwise, add one to the text length
-    if (x === 8) {
-        count.textContent = text.length - 1;
-    } else {
-        count.textContent = 1 + text.length;
-    }
-    
-    
-    
-    
-    
+//gets the character counter on the page
+var count = document.querySelector('#character-count');
+
+// listens for changes to textArea
+textArea.addEventListener('input', function(event){
+    //display the character counter
+    count.textContent = text.value.length;
+
 }, false);
+
+
+/* CODE BELOW IS MY OLD CODE aka: what was I thinking? LOL*/
+// //listens for key pressing in the textBox
+// textArea.addEventListener('keydown', function (event){
+//     //gets value of textbox
+//     var text = textArea.value;
+    
+//     //grabs keycode of key being pressed
+//     var x = event.keyCode;
+    
+//     //if x equals 8 (the delete key) minus one from the text length
+//     //otherwise, add one to the text length
+//     if (x === 8) {
+//         count.textContent = text.length - 1;
+//     } else {
+//         count.textContent = 1 + text.length;
+//     }
+    
+    
+    
+    
+    
+//  }, false);
